3e6dfe22573b3663f868d044084a63811341554e
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-15 20:53:41 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,10 @@
 require "pry"
 class Hash
   def keys_of(*args)
-    # new_arr = []
-    if args.include?(v)
-    args.map do |k,v| 
-      # if args == opts
-
-        new_arr << k
-      end
-   end
-  # return new_arr
+    
+    map do |k,v|
+      args.include?(v)
+    end
+    
   end
 end
